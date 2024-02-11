ba8334f208b63b05d107fb73188890b05d45413c
Kraust <secretdragoon@gmail.com>
2024-02-11 11:45:39 -0500

    Added default storage backend

--- OSCR_django/OSCR_django/settings.py
+++ OSCR_django/OSCR_django/settings.py
@@ -158,6 +158,9 @@ LOGGING = {
 }
 
 STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
