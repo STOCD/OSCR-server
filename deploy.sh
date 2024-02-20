498be4f561d117986233d6464c99a988a7264dde
Kraust <secretdragoon@gmail.com>
2024-02-19 20:48:28 -0500

    Updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-python3 -m pip install requirements.txt
+python3 -m pip install -r requirements.txt
 
-python3 manage.py migrate --noinput
-python3 manage.py collectstatic --clear --noinput
+python3 OSCR_django/manage.py migrate --noinput
+python3 OSCR_django/manage.py collectstatic --clear --noinput
 
 # Load the ladder information.
-python3 manage.py loaddata ladder/fixtures/ladders.json
+python3 OSCR_django/manage.py loaddata ladder/fixtures/ladders.json
