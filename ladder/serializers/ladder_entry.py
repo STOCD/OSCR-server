84e63c34e1f7766c26fae1ce20d969a9328998ff
Kraust <secretdragoon@gmail.com>
2024-02-26 17:51:20 -0500

    Add ladder rank.

--- ladder/serializers/ladder_entry.py
+++ ladder/serializers/ladder_entry.py
@@ -8,10 +8,17 @@ class LadderEntrySerializer(serializers.ModelSerializer):
     """LadderEntry Serializer"""
 
     date = serializers.SerializerMethodField(read_only=True)
+    rank = serializers.SerializerMethodField(read_only=True)
 
     def get_date(self, obj):
         """Return the date of the combat log"""
         return obj.combatlog.metadata.date_time
+
+    def get_rank(self, obj):
+        """Return the rank of the ladder entry with respect to the ladder"""
+        key = f"data__{obj.ladder.metric}__gte"
+        flt = {key: obj.data.get(obj.ladder.metric, 0)}
+        return LadderEntry.objects.filter(ladder=obj.ladder).filter(**flt).count()
 
     class Meta:
         model = LadderEntry
