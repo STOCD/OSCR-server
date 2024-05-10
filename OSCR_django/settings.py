f2320dfc8bcbb82cd66f4ab18196c7874cc07a46
Kraust <secretdragoon@gmail.com>
2024-05-10 15:49:22 -0400

    STATIC_HOST + STATIC_URL  (#30)
    * Working on a landing page.
    
    * Update STATIC_URL to get it from env
    
    * Try using STATIC_HOST as well.
    
    * STATIC_HOST + STATIC_URL

--- OSCR_django/settings.py
+++ OSCR_django/settings.py
@@ -156,7 +156,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_HOST = os.environ.get("STATIC_HOST", "")
-STATIC_URL = os.environ.get("STATIC_URL", "static/")
+STATIC_URL = STATIC_HOST + os.environ.get("STATIC_URL", "static/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
