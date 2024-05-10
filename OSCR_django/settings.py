b44c8186780cc5fd887360ba72017a60d0a72828
Kraust <secretdragoon@gmail.com>
2024-05-10 15:41:13 -0400

    Update STATIC_URL to get it from env  (#28)
    * Working on a landing page.
    
    * Update STATIC_URL to get it from env

--- OSCR_django/settings.py
+++ OSCR_django/settings.py
@@ -155,8 +155,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = "static/"
+STATIC_URL = os.environ.get("STATIC_URL", "static/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
