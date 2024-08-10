122e0683e93418502ee24265e5d21f031a2f2eca
Kraust <secretdragoon@gmail.com>
2024-08-10 19:57:49 -0400

    Fixup Meta

--- combatlog/templatetags/combatlog.py
+++ combatlog/templatetags/combatlog.py
@@ -16,6 +16,18 @@ def summary(instance):
     res.append(instance.metadata.map)
     res.append(f"{instance.metadata.difficulty} Difficulty")
     res.append("DPS")
+    players = []
     for _, player in instance.metadata.summary:
-        res.append(f"{player['handle']} - {int(player['DPS'])}")
+        print(player)
+        players.append(
+            {
+                "name": player["handle"],
+                "DPS": int(player["DPS"]),
+            }
+        )
+
+    players = sorted(players, reverse=True, key=lambda player: player["DPS"])
+    for player in players:
+        res.append(f"{player['name']} - {int(player['DPS']):,}")
+
     return " | ".join(res)
